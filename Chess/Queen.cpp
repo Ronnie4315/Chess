3ff5bd7fb72f8026a48bf767e82004b715f53d83
Ronnie4315 <aaronper25@gmail.com>
2025-03-30 19:09:01 -0400

    Finished All Pieces
    Need to do checkmate and alternating colors for moves

--- Chess/Queen.cpp
+++ Chess/Queen.cpp
@@ -12,11 +12,45 @@ char Queen::GetPiece() const{
 bool Queen::LegalSquare(int IntRow, int IntCol, int FinalRow, int FinalCol, BasePiece* Board[8][8]) const {
 	int roffset;
 	int coffset;
+	int direction;
 	roffset = FinalRow - IntRow;
 	coffset = FinalCol - IntCol;
+	int rdirection = (roffset > 0) ? 1 : -1;
+	int cdirection = (coffset > 0) ? 1 : -1;
+	if (abs(roffset) == abs(coffset)) {
+		for (int i = IntRow + rdirection; i != FinalRow; i = i + rdirection) {
+			if (Board[i][IntCol + cdirection] != nullptr) {
+				return false;
+			}
+			cdirection = cdirection + cdirection;
+		}
+		if (Board[FinalRow][FinalCol] == nullptr || Board[FinalRow][FinalCol]->GetColor() != this->GetColor()) {
+			return true;
+		}
+	}
+	if (IntRow == FinalRow) {
+		direction = (FinalCol - IntCol > 0) ? 1 : -1;
+		for (int i = IntCol + direction; i != FinalCol; i = i + direction) {
+			if (Board[IntRow][i] != nullptr) {
+				return false;
+			}
+		}
 
-	if (FinalRow=IntRow||FinalCol==IntCol|| abs(roffset) == abs(coffset)) {
+	}
+	if (IntCol == FinalCol) {
+		direction = (FinalRow - IntRow > 0) ? 1 : -1;
+		for (int i = IntRow + direction; i != FinalRow; i = i + direction) {
+			if (Board[i][IntCol] != nullptr) {
+				return false;
+			}
+		}
+	}
+	if ((FinalRow == IntRow + roffset && IntCol == FinalCol) && (Board[FinalRow][FinalCol] == nullptr || Board[FinalRow][FinalCol]->GetColor() != this->GetColor())) {
 		return true;
 	}
+	if (FinalCol == IntCol + coffset && IntRow == FinalRow && (Board[FinalRow][FinalCol] == nullptr || Board[FinalRow][FinalCol]->GetColor() != this->GetColor())) {
+		return true;
+	}
+
 	return false;
 }
