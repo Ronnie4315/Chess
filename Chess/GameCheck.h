445a3395c576ab9d92319a392d776e0457956804
IsamEL430 <isamlabadi@gmail.com>
2025-03-31 16:53:41 -0400

    Update GameCheck.h

--- Chess/GameCheck.h
+++ Chess/GameCheck.h
@@ -1,5 +1,9 @@
 #pragma once
-class GameCheck
-{
+#include "BasePiece.h"
+
+class GameCheck {
+public:
+    static bool isInCheck(char kingColor, BasePiece* Board[8][8]);
+    static bool isCheckMate(char kingColor, BasePiece* Board[8][8]);
 };
 
