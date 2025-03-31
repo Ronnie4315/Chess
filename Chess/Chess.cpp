457ab010b8de69f7391609560c014506701439be
franco32652 <dinatalefranco5@gmail.com>
2025-03-30 20:41:38 -0400

    Update main with turn tracking
    only allows you to move the right color on a specific turn

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     Board G1;  // Create a Board object
     int choice;
+    int turn = 0;  // Track turns (even = white, odd = black)
 
     while (true) {
         // Display a simple menu
@@ -22,8 +23,24 @@ int main() {
 
             // Game loop to allow repeated moves
             while (true) {
-                
-                G1.Move();  // Move a piece 
+                // Determine whose turn it is
+                char currentPlayer;
+                if (turn % 2 == 0) {
+                    currentPlayer = 'W';  // White's turn
+                    cout << "White's turn." << endl;
+                }
+                else {
+                    currentPlayer = 'B';  // Black's turn
+                    cout << "Black's turn." << endl;
+                }
+
+                // Make the move and check validity
+                bool validMove = G1.Move(currentPlayer);
+
+                // If the move was successful, switch turn
+                if (validMove) {
+                    turn++;  // Only increment if the move was valid
+                }
 
                 // Display the board after the move
                 cout << "\nBoard After Move:" << endl;
