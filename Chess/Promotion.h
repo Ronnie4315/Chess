4393f5ab9192a2bc7997fa2665c05eaef5c6b000
franco32652 <dinatalefranco5@gmail.com>
2025-03-31 14:06:30 -0400

    Update Promotion.h

--- Chess/Promotion.h
+++ Chess/Promotion.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "BasePiece.h"
+#include "Queen.h"
+#include "Rook.h"
+#include "Bishop.h"
+#include "Knight.h"
+#include <iostream> 
+
 class Promotion
 {
+
+public:
+
+	static BasePiece* PromotePawn(char color); 
+
 };
 
